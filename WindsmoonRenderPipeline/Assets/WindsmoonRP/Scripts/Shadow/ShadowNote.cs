896cf6238a98f7c495549ca895bf3f8f14e6e4dc
windsmoon <smwindsmoon@gmail.com>
2020-03-17 13:32:04 +0800

    note #6 add shadow note

---
+++ WindsmoonRenderPipeline/Assets/WindsmoonRP/Scripts/Shadow/ShadowNote.cs
@@ -0,0 +1,3 @@
+// shadow acne :
+// increasing the atlas size decreases the world-space size of the texels, so the acne artifacts get smaller
+// however, the amount of artifacts also increases, so the problem cannot be solved by simply increasing the atlas size
