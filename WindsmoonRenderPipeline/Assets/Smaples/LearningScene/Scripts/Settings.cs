8d41165934a8f0a4c60412d36fc87e6368861ee4
windsmoon <smwindsmoon@gmail.com>
2020-08-23 20:22:40 +0800

    learn unity LOD and reflection #10

---
+++ WindsmoonRenderPipeline/Assets/Smaples/LearningScene/Scripts/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    #region fields
+    [SerializeField]
+    private float crossFadeTime = 0.5f;
+    #endregion
+
+    #region unity methods
+    private void OnValidate()
+    {
+        LODGroup.crossFadeAnimationDuration = crossFadeTime;
+    }
+    #endregion
+}
