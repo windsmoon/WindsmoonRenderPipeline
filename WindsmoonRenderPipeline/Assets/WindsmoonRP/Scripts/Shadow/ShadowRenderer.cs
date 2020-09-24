58969a8d2024ab4a75aa913151212195185c933a
windsmoon <smwindsmoon@gmail.com>
2020-09-25 01:20:37 +0800

    post processing #14Point and Spot Shadows #13 re point

--- WindsmoonRenderPipeline/Assets/WindsmoonRP/Scripts/Shadow/ShadowRenderer.cs
+++ WindsmoonRenderPipeline/Assets/WindsmoonRP/Scripts/Shadow/ShadowRenderer.cs
@@ -267,6 +267,7 @@ namespace WindsmoonRP.Shadow
                 else
                 {
                     RenderSpotShadow(i, splitCount, tileSize);
+                    i += 1;
                 }
             }
             
