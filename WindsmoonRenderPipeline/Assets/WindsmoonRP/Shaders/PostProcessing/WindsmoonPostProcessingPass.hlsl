ce87d137ec3c39f13017dfdacaa934a77551f70b
windsmoon <smwindsmoon@gmail.com>
2020-09-26 16:14:39 +0800

    post processing #14

--- WindsmoonRenderPipeline/Assets/WindsmoonRP/Shaders/PostProcessing/WindsmoonPostProcessingPass.hlsl
+++ WindsmoonRenderPipeline/Assets/WindsmoonRP/Shaders/PostProcessing/WindsmoonPostProcessingPass.hlsl
@@ -1,6 +1,11 @@
 #ifndef WINDSMOON_POST_PROCESSING_INCLUDED
 #define WINDSMOON_POST_PROCESSING_INCLUDED
 
+TEXTURE2D(_PostProcessingSource);
+SAMPLER(sampler_linear_clamp);
+
+float4 _ProjectionParams; // x : if x is less than 0, the v is from top to bottom
+
 struct Varyings
 {
     float4 positionCS: SV_POSITION;
@@ -12,12 +17,24 @@ Varyings PostProcessingVertex(uint vertexID : SV_VertexID) // the order is (-1, 
     Varyings output;
     output.positionCS = float4(vertexID <= 1 ? -1.0 : 3.0, vertexID == 1 ? 3.0: -1.0, 0.0, 1.0);
     output.uv = float2(vertexID <= 1 ? 0.0 : 2.0, vertexID == 1 ? 2.0 : 0.0);
+
+    if (_ProjectionParams.x < 0.0)
+    {
+        output.uv.y = 1.0 - output.uv.y;
+    }
+    
     return output;
+}
+
+float4 GetSource(float2 uv)
+{
+    return SAMPLE_TEXTURE2D(_PostProcessingSource, sampler_linear_clamp, uv);
 }
 
 float4 CopyPassFragment(Varyings input) : SV_TARGET
 {
-    return float4(input.uv, 0.0, 1.0);
+    // return float4(input.uv, 0.0, 1.0);
+    return GetSource(input.uv);
 }
 
 #endif
